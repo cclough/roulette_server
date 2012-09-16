0c222f15cb16d5ac96a56d18a66307041e2f116e
Christian Clough <christian.clough@gmail.com>
2012-09-16 14:22:41 +0100

    add back io.sockets.emit lines

---
+++ roulette.js
@@ -0,0 +1,51 @@
+
+var app = require('http').createServer();
+var io = require('socket.io');
+
+io = io.listen(app);
+io.configure(function(){
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+  io.set("close timeout", 10);
+  io.set("log level", 1);
+})
+
+// usernames which are currently connected to the chat
+var usernames = {};
+
+io.sockets.on('connection', function (socket) {
+
+	// // when the client emits 'sendchat', this listens and executes
+	// socket.on('sendchat', function (data) {
+	// 	// we tell the client to execute 'updatechat' with 2 parameters
+	// 	io.sockets.emit('updatechat', socket.username, data);
+	// });
+
+	// when the client emits 'adduser', this listens and executes
+	socket.on('adduser', function(username){
+		// we store the username in the socket session for this client
+		socket.username = username;
+		// add the client's username to the global list
+		usernames[username] = username;
+		// echo to client they've connected
+		socket.emit('updatelog', 'SERVER', 'you have connected');
+		// echo globally (all clients) that a person has connected
+		socket.broadcast.emit('updatelog', 'SERVER', username + ' has connected');
+		// update the list of users in chat, client-side
+		//io.sockets.emit('updateusers', usernames);
+	});
+
+	// when the user disconnects.. perform this
+	socket.on('disconnect', function(){
+		// remove the username from global usernames list
+		delete usernames[socket.username];
+		// update list of users in chat, client-side
+		//io.sockets.emit('updateusers', usernames);
+		// echo globally that this client has left
+		socket.broadcast.emit('updatelog', 'SERVER', socket.username + ' has disconnected');
+	});
+});
+
+//this line is necessary for heroku
+var port = process.env.PORT || 5001;
+app.listen(port);
